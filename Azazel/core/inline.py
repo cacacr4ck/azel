cdbb0b4f37f97de7075e47c95dd3ba0b3f80d119
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2024-01-18 16:00:41 +0700

    Update inline.py

--- Azazel/core/inline.py
+++ Azazel/core/inline.py
@@ -53,7 +53,7 @@ def cb_wrapper(func):
         users = list_users
         if cb.from_user.id not in users:
             await cb.answer(
-                "bikin sendiri ngapa, bikinnya di @GeezRam",
+                "bikin sendiri ngapa, bikinnya di @zull_probot",
                 cache_time=0,
                 show_alert=True,
             )
@@ -83,7 +83,7 @@ def inline_wrapper(func):
                 results=[
                     (
                         InlineQueryResultArticle(
-                            title="bikin sendiri ngpa, bikinnya di @KynanSupport",
+                            title="bikin sendiri ngpa, bikinnya di @zull_probot",
                             input_message_content=InputTextMessageContent(
                                 "You cannot access this Bot"
                             ),
